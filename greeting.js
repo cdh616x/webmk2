72f4e6f3f75a2f28e1f7005dd68d1dca7a811563
cdh666x <91150236+cdh666x@users.noreply.github.com>
2021-11-09 22:02:27 -0500

    added more js

--- greeting.js
+++ greeting.js
@@ -3,7 +3,23 @@ function greet() {
   var whatDay = day.getDay();
   var hour = day.getHours();
 
-  if (hour < 12) {
+  if (hour < 6) {
+    if (whatDay === 0) {
+      $("#greeting").text("Good (early) morning; happy Sunday!");
+    } else if (whatDay === 1) {
+      $("#greeting").text("Good (early) morning; happy Monday!");
+    } else if (whatDay === 2) {
+      $("#greeting").text("Good (early) morning; happy Tuesday!");
+    } else if (whatDay === 3) {
+      $("#greeting").text("Good (early) morning; happy Wednesday!");
+    } else if (whatDay === 4) {
+      $("#greeting").text("Good (early) morning; happy Thursday!");
+    } else if (whatDay === 5) {
+      $("#greeting").text("Good (early) morning; happy Friday!");
+    } else {
+      $("#greeting").text("Good (early) morning; happy Saturday!");
+    }
+  } else if (hour < 12) {
     if (whatDay === 0) {
       $("#greeting").text("Good morning; happy Sunday!");
     } else if (whatDay === 1) {
