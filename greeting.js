be4f3bfc302d4a7d91a68e63d45429ded63a7a4e
cdh666x <91150236+cdh666x@users.noreply.github.com>
2021-11-09 21:43:34 -0500

    added js file and specific day and relative time greeting

---
+++ greeting.js
@@ -0,0 +1,58 @@
+function greet() {
+  var day = new Date();
+  var whatDay = day.getDay();
+  var hour = day.getHours();
+
+  if (hour < 12) {
+    if (whatDay === 0) {
+      $("#greeting").text("Good morning; happy Sunday!");
+    } else if (whatDay === 1) {
+      $("#greeting").text("Good morning; happy Monday!");
+    } else if (whatDay === 2) {
+      $("#greeting").text("Good morning; happy Tuesday!");
+    } else if (whatDay === 3) {
+      $("#greeting").text("Good morning; happy Wednesday!");
+    } else if (whatDay === 4) {
+      $("#greeting").text("Good morning; happy Thursday!");
+    } else if (whatDay === 5) {
+      $("#greeting").text("Good morning; happy Friday!");
+    } else {
+      $("#greeting").text("Good morning; happy Saturday!");
+    }
+  } else if (hour < 18) {
+    if (whatDay === 0) {
+      $("#greeting").text("Good afternoon; happy Sunday!");
+    } else if (whatDay === 1) {
+      $("#greeting").text("Good afternoon; happy Monday!");
+    } else if (whatDay === 2) {
+      $("#greeting").text("Good afternoon; happy Tuesday!");
+    } else if (whatDay === 3) {
+      $("#greeting").text("Good afternoon; happy Wednesday!");
+    } else if (whatDay === 4) {
+      $("#greeting").text("Good afternoon; happy Thursday!");
+    } else if (whatDay === 5) {
+      $("#greeting").text("Good afternoon; happy Friday!");
+    } else {
+      $("#greeting").text("Good afternoon; happy Saturday!");
+    }
+  } else {
+    if (whatDay === 0) {
+      $("#greeting").text("Good evening; happy Sunday!");
+    } else if (whatDay === 1) {
+      $("#greeting").text("Good evening; happy Monday!");
+    } else if (whatDay === 2) {
+      $("#greeting").text("Good evening; happy Tuesday!");
+    } else if (whatDay === 3) {
+      $("#greeting").text("Good evening; happy Wednesday!");
+    } else if (whatDay === 4) {
+      $("#greeting").text("Good evening; happy Thursday!");
+    } else if (whatDay === 5) {
+      $("#greeting").text("Good evening; happy Friday!");
+    } else {
+      $("#greeting").text("Good evening; happy Saturday!");
+    }
+  }
+
+}
+
+greet();
